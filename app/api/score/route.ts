77a79d21f51fc578ca6aca7bfdd9c61ee410702c
Aayushman-nvm <aayushman.jha001@gmail.com>
2026-05-16 00:43:57 +0530

    score

--- app/api/score/route.ts
+++ app/api/score/route.ts
@@ -6,7 +6,7 @@ import type { RedisThreat, BlacklistItem } from "@/types/redis";
 import type { CloudflareTopOrigin, HijackIPEntry, CloudflareTopAttack } from "@/types/types"
 
 export const dynamic = "force-dynamic";
-export const maxDuration = 30;
+export const maxDuration = 300;
 
 export async function POST(request: Request) {
   try {
